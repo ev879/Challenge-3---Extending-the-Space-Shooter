4a6d249e9608a5eeff3161c5165395c2f76f81c1
ev879 <42782232+ev879@users.noreply.github.com>
2018-11-08 02:01:08 -0500

    Add files via upload

---
+++ Scripts/WeaponController.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class WeaponController : MonoBehaviour {
+    public GameObject shot;
+    public Transform shotSpawn;
+    public float fireRate;
+    public float delay;
+    private AudioSource sound;
+    void Start () {
+        sound = GetComponent<AudioSource>();
+        InvokeRepeating("Fire", delay, fireRate);
+	}
+	void Fire()
+    {
+        Instantiate(shot, shotSpawn.position, shotSpawn.rotation);
+        sound.Play();
+    }
+}
